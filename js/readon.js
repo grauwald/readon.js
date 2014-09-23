cfe1b5e6233c844a16facc193c0b06c98f3f34e0
Joseph Gray <josephgray@grauwald.com>
2014-09-22 17:52:46 -0700

    close button working

--- js/readon.js
+++ js/readon.js
@@ -21,12 +21,37 @@ ReadOn = function(){
 		
 		$slides = $j('#readon .slide');
 		
-		$slides.each(function(){
+		$slides.each(function(index){
+			$j(this).data('index', index);
+		
+			// add close button
+			var $closeBtn = $j('<div class="closeBtn">X</div>');
+			$closeBtn.click(destroySlide);
+			$j(this).append($closeBtn);
+		
+			// position the element
 			$j(this).css('left', slidesCurrentX);
 			slidesCurrentX += $j(this).outerWidth() + slidesMargin;
 		});
+	}
+	
+	var destroySlide = function(){
+		var $slide = $j(this).parent(); // close button's parent element
 		
+		slideWidth = $slide.outerWidth();  // store slide's width
+		var slideIndex = $slide.data('index'); // store slide's index
 		
+		$slide.remove(); // actually remove the slide
+		
+		for(var index=slideIndex+1; index<$slides.length; index++){ // move all slides here after
+			var $nextSlide = $slides.eq(index);
+			var left = $nextSlide.position().left;
+			var pos = left-(slideWidth+slidesMargin);
+			$nextSlide.css('left', pos);
+			
+			// set the currentIndex to whoever's at 0px
+			if(pos == 0) currentIndex = $nextSlide.data('index');
+		}
 	}
 	
 	var nextSlide = function(){
@@ -35,13 +60,14 @@ ReadOn = function(){
 
 			$slides.each(function(){
 				var left = $j(this).position().left;
-				console.log(left);
-				$j(this).css('left', left-(slideWidth+slidesMargin));
+				var pos = left-(slideWidth+slidesMargin);
+				$j(this).css('left', pos);
 			});
 		
 			currentIndex++;
 		}
 	}
+	
 	var lastSlide = function(){
 		if(currentIndex != 0) {
 			currentIndex--;
@@ -50,8 +76,8 @@ ReadOn = function(){
 
 			$slides.each(function(){
 				var left = $j(this).position().left;
-				console.log(left);
-				$j(this).css('left', left+(slideWidth+slidesMargin));
+				var pos = left+(slideWidth+slidesMargin);
+				$j(this).css('left', pos);
 			});
 		
 		}
